f92883c6e9949219f30b7e0703bc448d0ea5f39b
jenniferelkhoury0 <jenniferelkhoury0@gmail.com>
2026-05-04 16:36:33 +0300

    Update loginlogic.php

--- loginlogic.php
+++ loginlogic.php
@@ -5,7 +5,6 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "movie_db";
-
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
