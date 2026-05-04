ba2ff8088c301b9ec349e05652878caedd190f72
jenniferelkhoury0 <jenniferelkhoury0@gmail.com>
2026-05-04 16:42:08 +0300

    Update signuplogic.php

--- signuplogic.php
+++ signuplogic.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "movies_db";
+$dbname = "movie_db";
 $error = ""; // Initialize error variable
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
